9a180707ea4f6fdad5551093fac95dcea924b6ac
lexicalunit <lexicalunit@lexicalunit.com>
2013-03-05 15:50:35 -0600

    [nanodbc] deleted outdated documentation.

--- doc/search/all_77.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['what',['what',['../classnanodbc_1_1type__incompatible__error.html#a6baad743632035b2065646e0d212c601',1,'nanodbc::type_incompatible_error::what()'],['../classnanodbc_1_1null__access__error.html#ab00e8b0dde3526c1c0c7c5c1ba144192',1,'nanodbc::null_access_error::what()'],['../classnanodbc_1_1index__range__error.html#a829add4c047f618483f011c44398a460',1,'nanodbc::index_range_error::what()'],['../classnanodbc_1_1database__error.html#ac39a8d1a017168e924580ae9bff31db6',1,'nanodbc::database_error::what()']]]
-];
--- doc/search/enums_63.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['column_5fdatatype',['column_datatype',['../group__utility.html#ga47d1e22a8d3d3d06fe137595e1a1a8bd',1,'nanodbc']]]
-];
