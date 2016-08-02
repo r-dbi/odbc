8c147876ec9d548272df03322423ff22a8ee7d01
Jim Hester <james.f.hester@gmail.com>
2016-08-02 13:29:16 -0400

    Remove bad file

--- src/cpp_odbc/connection.cpp
+++
@@ -1,7 +0,0 @@
-#include "Rcpp.h"
-#include "cpp_odbc/make_environment.h"
-
-Rcpp::XPtr<connection> connect(const std::string& connection_string) {
-  auto environment = cpp_odbc::make_environment();
-  return Rcpp::XPtr<std::shared_ptr<connection const>>(environment->make_connection(connection_string));
-}
