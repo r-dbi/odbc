6d13628a2c41d25df768efb17b10f13e43603b53
Jim Hester <james.f.hester@gmail.com>
2016-08-12 13:02:04 -0400

    No ned to skip the stress tests

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,7 +1,6 @@
 DBItest::make_context(odbconnect(), list("DSN=database1"), tweaks = DBItest::tweaks(), name = "odbconnect")
 DBItest::test_all(c(
   "package_name",
-  ".*stress.*", # TODO: Enable
   "quote_identifier_not_vectorized",
   "invalid_query",
   "command_query",
