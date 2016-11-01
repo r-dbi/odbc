539a3e7efaccfdf2631b43c915e42faf556914ed
Jim Hester <james.f.hester@gmail.com>
2016-11-01 09:50:18 -0400

    Add missing helper

---
+++ tests/testthat/helper.R
@@ -0,0 +1,24 @@
+test_roundtrip <- function() {
+  ctx <- DBItest:::get_default_context()
+  context(paste0("roundtrip[", ctx$name, "]"))
+  test_that("round tripping data.frames works", {
+    library(DBI)
+    on.exit(dbRemoveTable(con, "it"))
+    con <- DBItest:::connect(ctx)
+    it <- iris
+    set.seed(42)
+    it$Petal.Length <- as.POSIXct(as.numeric(iris$Petal.Length * 10), origin = "2016-01-01", tz = "GMT")
+    it$Petal.Width <- as.integer(iris$Petal.Width * 100)
+    it$Sepal.Width <- as.Date(iris$Sepal.Width * 100, origin = Sys.time())
+    it$Logical <- sample(c(TRUE, FALSE), size = nrow(it), replace = T)
+    it$Species <- as.character(it$Species)
+    add_na <- function(x) { x[sample(length(x))[1:15]] <- NA; x }
+    it[] <- lapply(it, add_na)
+    it$Raw <- lapply(seq_len(NROW(it)), function(x) as.raw(sample(0:100, size = sample(0:25, 1))))
+
+    dbWriteTable(con, "it", it, overwrite = TRUE)
+    res <- dbReadTable(con, "it")
+    res$Raw = unclass(res$Raw)
+    expect_equal(it, res)
+  })
+}
