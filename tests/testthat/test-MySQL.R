32f78fe91e5aac665af5f95b94044e463be44fae
Jim Hester <james.f.hester@gmail.com>
2016-11-03 16:00:17 -0400

    Turn on many more tests for MySQL

--- tests/testthat/test-MySQL.R
+++ tests/testthat/test-MySQL.R
@@ -1,46 +1,32 @@
 if (.Platform$OS.type != "windows") {
-  DBItest::make_context(odbconnect(), list(dsn = "MySQL"), tweaks = DBItest::tweaks(), name = "MySQL")
+  ctx <- DBItest::make_context(odbconnect(), list(dsn = "MySQL"), tweaks = DBItest::tweaks(), name = "MySQL")
 
 DBItest::test_getting_started(c(
-    "package_name",
+    "package_name", # Not an error
     NULL))
 DBItest::test_driver()
 DBItest::test_connection(c(
     NULL))
-DBItest::test_result(
-  c("stale_result_warning",
-    "fetch_no_return_value",
-    "data_numeric.*",
-    "data_logical",
-    "data_logical_null_.*",
-    "data_logical_int.*",
-    "data_numeric.*", # Numeric types with high precision are converted to strings
-    "data_64_bit.*", # Numeric types with high precision are converted to strings
-    "data_character.*", # Character encodings are not preserved (on R side)
-    "data_raw.*",
-    "data_type_connection",
-    "data_time.*", # timezones not quite working right yet
-    "data_date.*",
-    NULL)) # date data has class in entire row rather than for each item.
-DBItest::test_sql(c(
-    "quote_identifier_not_vectorized", # https://github.com/rstats-db/DBI/issues/71
-    "append_table_error",
-    "temporary_table",
-    "roundtrip_logical",
-    "roundtrip_logical_int",
-    "roundtrip_null",
-    "roundtrip_64_bit",
-    "roundtrip_numeric_special",
-    "roundtrip_raw",
-    "roundtrip_date",
-    "roundtrip_timestamp",
-    "roundtrip_rownames",
+DBItest::test_result(c(
+    "data_logical($|_.+)", # Not an error, PostgreSQL has a logical data type
+    "data_64_bit.*", # TODO
+    "data_raw.*", # cast(1 bytea) is not valid `cannot cast type integer to bytea`
+    "^data_time$", "^data_time_.*", # time objects not supported
+    "^data_timestamp.*", # MySQL converts the timestamps from local times, so they roundtrip unexpectedly
+    "^data_timestamp_utc.*", # syntax not supported
+    "^data_timestamp_parens.*", # syntax not supported
     NULL))
+DBItest::test_sql(c(
+    "quote_identifier_not_vectorized", # Can't implement until https://github.com/rstats-db/DBI/issues/71 is closed
+    "roundtrip_logical", # Not an error, PostgreSQL has a logical data type
+    "roundtrip_64_bit", # TODO
+    "roundtrip_numeric_special", # #6
+  NULL))
 DBItest::test_meta(c(
-    "column_info",
-    "bind_empty",
     NULL))
 DBItest::test_compliance(c(
-    "read_only",
+    "read_only", # Setting SQL_MODE_READ_ONLY is not supported in most DBs, so ignoring.
     NULL))
+
+  test_roundtrip("Logical")
 }
