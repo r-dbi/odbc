9805ac7c91c99959cb6a167e383b8a8fec554791
Jim Hester <james.f.hester@gmail.com>
2016-11-07 17:38:44 -0500

    Turn on more tests for SQL server

--- tests/testthat/test-SQLServer.R
+++ tests/testthat/test-SQLServer.R
@@ -2,53 +2,43 @@ if (.Platform$OS.type == "windows") {
   if (identical(Sys.getenv("APPVEYOR"), "True")) {
     DBItest::make_context(odbconnect(), list(Driver ="{SQL Server}", "Server" = "(local)\\SQL2014", "Database" = "master", "User ID" = "sa", "Password" = "Password12"), tweaks = DBItest::tweaks(), name = "SQLServer")
   } else {
-    DBItest::make_context(odbconnect(), list(dsn = "test"), tweaks = DBItest::tweaks(), name = "SQLServer")
+    ctx <- DBItest::make_context(odbconnect(), list(dsn = "test"), tweaks = DBItest::tweaks(), name = "SQLServer")
   }
 
 DBItest::test_getting_started(c(
-    "package_name",
+    "package_name", # Not an error
     NULL))
-DBItest::test_driver(c(
-  NULL
-))
+DBItest::test_driver()
 DBItest::test_connection(c(
-    "stress.*",
     NULL))
-DBItest::test_result(
-  c("stale_result_warning",
-    "fetch_no_return_value",
-    "data_logical",
-    "data_logical_null_.*",
-    "data_logical_int.*",
-    "data_numeric.*", # Numeric types with high precision are converted to strings
-    "data_64_bit.*", # Numeric types with high precision are converted to strings
-    "data_raw.*",
-    "data_type_connection",
-    "data_time.*", # timezones not quite working right yet
-    "data_date.*",
-    NULL)) # date data has class in entire row rather than for each item.
+DBItest::test_result(c(
+    "data_logical$", # Not an error
+    "data_logical_.*", # Not an error
+
+    "data_date.*", # Date not a builtin function name
+    "data_64_bit.*", # TODO
+    "data_raw.*", # cast(1 bytea) is not valid `cannot cast type integer to bytea`
+    "^data_time$", "^data_time_.*", # time objects not supported
+    "^data_timestamp.*", # syntax not supported
+    "^data_timestamp_parens.*", # syntax not supported
+    NULL))
 DBItest::test_sql(c(
-    "quote_identifier_not_vectorized", # https://github.com/rstats-db/DBI/issues/71
-    "append_table_error",
-    "temporary_table",
-    "roundtrip_logical",
-    "roundtrip_logical_int",
-    "roundtrip_null",
-    "roundtrip_64_bit",
-    "roundtrip_character",
-    "roundtrip_factor",
-    "roundtrip_numeric_special",
-    "roundtrip_raw",
-    "roundtrip_date",
-    "roundtrip_timestamp",
-    "roundtrip_rownames",
-    "roundtrip_quotes",
+    "quote_identifier_not_vectorized", # Can't implement until https://github.com/rstats-db/DBI/issues/71 is closed
+    "roundtrip_logical_int", # Not an error, PostgreSQL has a logical data type
+    "roundtrip_64_bit", # TODO
+    "temporary_table", # Unsupported
+    "roundtrip_numeric_special", # Unsupported
+    "roundtrip_character", # #10
+    "roundtrip_factor", # #10
+    "roundtrip_raw", # #10
+    "roundtrip_rownames", # #10
     NULL))
 DBItest::test_meta(c(
-    "column_info",
-    "bind_empty",
+  "bind_empty",
     NULL))
 DBItest::test_compliance(c(
-    "read_only",
+    "read_only", # Setting SQL_MODE_READ_ONLY is not supported in most DBs, so ignoring.
     NULL))
+
+#  test_roundtrip() #10
 }
