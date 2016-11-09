b81d5ade55bbd15aa6c47d1a14fecf90f86e4a98
Jim Hester <james.f.hester@gmail.com>
2016-11-09 10:46:33 -0500

    Ignore zzz.R and add tests for show methods

---
+++ tests/testthat/test-show.R
@@ -0,0 +1,39 @@
+context("show")
+
+test_that("show method works as expected with real connection", {
+  con <- dbConnect(odbconnect(), "PostgreSQL")
+
+  expect_output(show(con), "postgres@localhost")
+  expect_output(show(con), "Database: test_db")
+  expect_output(show(con), "PostgreSQL Version: ")
+})
+
+test_that("show method prints only host if no user is defined", {
+  con <- new("OdbconnectConnection")
+  with_mock(
+    dbGetInfo = function(x) c(servername = "localhost", username = "", dbname = "", dbms.name = "", db.version = ""),
+    dbIsValid = function(x) TRUE,
+    {
+      expect_output(show(con), " localhost")
+    })
+})
+
+test_that("show method prints DISCONNECTED if not valid", {
+  con <- new("OdbconnectConnection")
+  with_mock(
+    dbGetInfo = function(x) c(servername = "localhost", username = "", dbname = "", dbms.name = "", db.version = ""),
+    dbIsValid = function(x) FALSE,
+    {
+      expect_output(show(con), "  DISCONNECTED")
+    })
+})
+
+test_that("show method does not print server if it is not available", {
+  con <- new("OdbconnectConnection")
+  with_mock(
+    dbGetInfo = function(x) c(servername = "", username = "", dbname = "", dbms.name = "", db.version = ""),
+    dbIsValid = function(x) TRUE,
+    {
+      expect_output(show(con), "<OdbconnectConnection>$")
+    })
+})
