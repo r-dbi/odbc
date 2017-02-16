6c8dbe173c797dd20f5950f4e5db9292334d6159
Jim Hester <james.f.hester@gmail.com>
2017-02-16 14:48:05 -0500

    Set TZDIR on windows to R's timezone database

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,16 @@
-.onUnload <- function (libpath) { # nocov start
+ # nocov start
+.onUnload <- function (libpath) {
   gc() # Force garbage collection of connections
   library.dynam.unload("odbc", libpath)
-} # nocov end
+}
+
+.onLoad <- function(libname, pkgname) {
+  is_windows <- isTRUE(.Platform$OS.type == "windows")
+
+  # If TZDIR is not set we need to set it to R's timezone database on windows,
+  # we can use the standard timezone database elsewhere.
+  if (is_windows) {
+    if (!nzchar(Sys.getenv("TZDIR"))) Sys.setenv("TZDIR" = file.path(R.home(), "share", "zoneinfo"))
+  }
+}
+ # nocov end
