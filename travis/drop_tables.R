377f4f6c53a7424d414984aaa665e9ecc523d970
Jim Hester <james.f.hester@gmail.com>
2017-04-07 15:59:02 -0400

    Pass connection to dbRemoveTable

--- travis/drop_tables.R
+++ travis/drop_tables.R
@@ -3,7 +3,7 @@ library(DBI)
 devtools::install()
 
 pg <- dbConnect(odbc::odbc(), "PostgreSQL")
-lapply(dbListTables(pg), dbRemoveTable)
+lapply(dbListTables(pg), dbRemoveTable, conn = pg)
 
 msql <- dbConnect(odbc::odbc(), "MySQL")
-lapply(dbListTables(msql), dbRemoveTable)
+lapply(dbListTables(msql), dbRemoveTable, conn = mysql)
