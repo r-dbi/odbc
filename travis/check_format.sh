a556324da4441cc11d0db6c451c4ec5a233fea53
Jim Hester <james.f.hester@gmail.com>
2017-04-27 17:44:35 -0400

    Update format to use clang-format 3.9

--- travis/check_format.sh
+++ travis/check_format.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-set -e
-
 shopt -s extglob
-clang-format -i src/!(RcppExports).@(c|h|cpp) && git diff-files -U --exit-code
+
+clang_format_3_9=$(command -v clang-format-3.9)
+if [ ! -z "$clang_format_3_9" ]; then
+  clang_format=$clang_format_3_9
+else
+  clang_format=$(command -v clang-format)
+fi
+if [ -z "$clang_format" ]; then
+  echo "Could not find recent version of clang-format"
+  exit 1
+fi
+"$clang_format" -i src/!(RcppExports).@(c|h|cpp) && git diff-files -U --exit-code
