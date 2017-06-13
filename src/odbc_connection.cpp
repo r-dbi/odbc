ececc9a43aeee15b0f7f9ec95cdc3e8ac1213a9c
Jim Hester <james.f.hester@gmail.com>
2017-06-13 16:47:00 -0400

    Move definitions to cpp file

--- src/odbc_connection.cpp
+++ src/odbc_connection.cpp
@@ -1,7 +1,9 @@
 #include "odbc_connection.h"
 #include "odbc_result.h"
 
-void odbc::odbc_connection::set_current_result(odbc_result* r) {
+namespace odbc {
+
+void odbc_connection::set_current_result(odbc_result* r) {
   if (r == current_result_) {
     return;
   }
@@ -15,3 +17,55 @@ void odbc::odbc_connection::set_current_result(odbc_result* r) {
 
   current_result_ = r;
 }
+
+odbc_connection::odbc_connection(
+    std::string connection_string, std::string timezone, std::string encoding)
+    : current_result_(nullptr), encoding_(encoding) {
+
+  if (!cctz::load_time_zone(timezone, &timezone_)) {
+    Rcpp::stop("Error loading time zone (%s)", timezone);
+  }
+
+  try {
+    c_ = std::make_shared<nanodbc::connection>(connection_string);
+  } catch (nanodbc::database_error e) {
+    throw Rcpp::exception(e.what(), FALSE);
+  }
+}
+
+std::shared_ptr<nanodbc::connection> odbc_connection::connection() const {
+  return std::shared_ptr<nanodbc::connection>(c_);
+}
+
+void odbc_connection::begin() {
+  if (t_) {
+    Rcpp::stop("Double begin");
+  }
+  t_ = std::unique_ptr<nanodbc::transaction>(new nanodbc::transaction(*c_));
+}
+void odbc_connection::commit() {
+  if (!t_) {
+    Rcpp::stop("Commit without beginning transaction");
+  }
+  t_->commit();
+  t_.release();
+}
+void odbc_connection::rollback() const {
+  if (!t_) {
+    Rcpp::stop("Rollback without beginning transaction");
+  }
+  t_->rollback();
+}
+bool odbc_connection::has_active_result() const {
+  return current_result_ != nullptr;
+}
+bool odbc_connection::is_current_result(odbc_result* result) const {
+  return current_result_ == result;
+}
+bool odbc_connection::supports_transactions() const {
+  return c_->get_info<unsigned short>(SQL_TXN_CAPABLE) != SQL_TC_NONE;
+}
+
+cctz::time_zone odbc_connection::timezone() const { return timezone_; }
+std::string odbc_connection::encoding() const { return encoding_; }
+} // namespace odbc
--- src/odbc_connection.h
+++ src/odbc_connection.h
@@ -13,55 +13,21 @@ public:
   odbc_connection(
       std::string connection_string,
       std::string timezone = "UTC",
-      std::string encoding = "")
-      : current_result_(nullptr), encoding_(encoding) {
+      std::string encoding = "");
 
-    if (!cctz::load_time_zone(timezone, &timezone_)) {
-      Rcpp::stop("Error loading time zone (%s)", timezone);
-    }
+  std::shared_ptr<nanodbc::connection> connection() const;
 
-    try {
-      c_ = std::make_shared<nanodbc::connection>(connection_string);
-    } catch (nanodbc::database_error e) {
-      throw Rcpp::exception(e.what(), FALSE);
-    }
-  }
-
-  std::shared_ptr<nanodbc::connection> connection() const {
-    return std::shared_ptr<nanodbc::connection>(c_);
-  }
-
-  void begin() {
-    if (t_) {
-      Rcpp::stop("Double begin");
-    }
-    t_ = std::unique_ptr<nanodbc::transaction>(new nanodbc::transaction(*c_));
-  }
-  void commit() {
-    if (!t_) {
-      Rcpp::stop("Commit without beginning transaction");
-    }
-    t_->commit();
-    t_.release();
-  }
-  void rollback() const {
-    if (!t_) {
-      Rcpp::stop("Rollback without beginning transaction");
-    }
-    t_->rollback();
-  }
-  bool has_active_result() const { return current_result_ != nullptr; }
-  bool is_current_result(odbc_result* result) const {
-    return current_result_ == result;
-  }
-  bool supports_transactions() const {
-    return c_->get_info<unsigned short>(SQL_TXN_CAPABLE) != SQL_TC_NONE;
-  }
+  void begin();
+  void commit();
+  void rollback() const;
+  bool has_active_result() const;
+  bool is_current_result(odbc_result* result) const;
+  bool supports_transactions() const;
 
   void set_current_result(odbc_result* r);
 
-  cctz::time_zone timezone() const { return timezone_; }
-  std::string encoding() const { return encoding_; }
+  cctz::time_zone timezone() const;
+  std::string encoding() const;
 
 private:
   std::shared_ptr<nanodbc::connection> c_;
