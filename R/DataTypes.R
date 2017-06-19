6580a3ece079b8ef244cd62da834b192f8dec81b
Jim Hester <james.f.hester@gmail.com>
2017-06-19 13:16:21 -0400

    Add odbcDataType method for Oracle databases

--- R/DataTypes.R
+++ R/DataTypes.R
@@ -167,6 +167,32 @@ odbcDataType.default <- function(con, obj, ...) {
 }
 
 #' @export
+odbcDataType.Oracle <- function(con, obj, ...) {
+  odbc:::switch_type(obj,
+     factor = "VARCHAR(255)",
+
+     # No native oracle type for time
+     time = "VARCHAR(255)",
+
+     # There is a native type for date, but the default input format may not be
+     # ISO 8601, it is determined by NLS_DATE_FORMAT or derived from
+     # NLS_TERRITORY, so use character as a fallback.
+     date = "VARCHAR(255)",
+
+     # datetime errors with * character string is not in a standard unambiguous
+     # format, even with character input.
+#    datetime = "VARCHAR(255)",
+     binary = "BLOB",
+     integer = "INTEGER",
+     double = "BINARY_DOUBLE",
+     character = "VARCHAR(255)",
+     logical = "DECIMAL",
+     list = "VARCHAR(255)",
+     stop("Unsupported type", call. = FALSE)
+  )
+}
+
+#' @export
 `odbcDataType.SQLite` <- function(con, obj, ...) {
   switch_type(obj,
     factor = "TEXT",
