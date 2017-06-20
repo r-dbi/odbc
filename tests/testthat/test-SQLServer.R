1bd872644fade9127d9aa286297f26b571635ce2
Jim Hester <james.f.hester@gmail.com>
2017-06-20 13:40:22 -0400

    Fix location of encoding parameter

--- tests/testthat/test-SQLServer.R
+++ tests/testthat/test-SQLServer.R
@@ -1,7 +1,7 @@
 test_that("SQLServer", {
   skip_unless_has_test_db({
     if (identical(Sys.getenv("APPVEYOR"), "True")) {
-      DBItest::make_context(odbc(), list(Driver ="{SQL Server}", "Server" = "(local)\\SQL2014", "Database" = "master", "User ID" = "sa", "Password" = "Password12"), tweaks = DBItest::tweaks(temporary_tables = FALSE), name = "SQLServer", encoding = "latin1")
+      DBItest::make_context(odbc(), list(Driver ="{SQL Server}", "Server" = "(local)\\SQL2014", "Database" = "master", "User ID" = "sa", "Password" = "Password12", encoding = "latin1"), tweaks = DBItest::tweaks(temporary_tables = FALSE), name = "SQLServer")
     } else {
       DBItest::make_context(odbc(), list(dsn = "SQLServer", UID="testuser", PWD="test"), tweaks = DBItest::tweaks(temporary_tables = FALSE), name = "SQLServer")
     }
