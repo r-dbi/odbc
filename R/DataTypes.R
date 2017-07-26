5acc55350e0664002d6281f4659c089911fbd7c3
Jim Hester <james.f.hester@gmail.com>
2017-07-26 11:57:50 -0400

    Add redshift data types

--- R/DataTypes.R
+++ R/DataTypes.R
@@ -64,6 +64,24 @@ odbcDataType.default <- function(con, obj, ...) {
   )
 }
 
+
+#' @export
+`odbcDataType.Redshift` <- function(con, obj, ...) {
+  switch_type(obj,
+    factor = "VARCHAR(255)",
+    datetime = "TIMESTAMP",
+    date = "DATE",
+    integer = "INTEGER",
+    double = "DOUBLE PRECISION",
+    character = "VARCHAR(255)",
+    logical = "BOOLEAN",
+    list = "VARCHAR(255)",
+    time = ,
+    binary =,
+    stop("Unsupported type", call. = FALSE)
+  )
+}
+
 #' @export
 `odbcDataType.Spark SQL` <- function(con, obj, ...) {
   switch_type(obj,
