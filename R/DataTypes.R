37373dd15d2abe1045a742a06cdcd4ac8725a827
James Blair <james.m.blair09@gmail.com>
2018-11-01 08:05:06 -0600

    Add time type and remove list type for BigQuery

--- R/DataTypes.R
+++ R/DataTypes.R
@@ -251,13 +251,13 @@ odbcDataType.Oracle <- function(con, obj, ...) {
   switch_type(obj,
     factor = "STRING",
     datetime = "TIMESTAMP",
+    time = "TIME",
     date = "DATE",
     binary = "BYTES",
     integer = "INT64",
     double = "FLOAT64",
     character = "STRING",
     logical = "BOOL",
-    list = "STRING",
     stop("Unsupported type", call. = FALSE)
   )
 }
