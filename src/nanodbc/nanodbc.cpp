5095a38066aa275c4b7d82ccbadb45dbbcd282c9
Jim Hester <james.f.hester@gmail.com>
2019-11-21 10:52:59 -0500

    Port of https://github.com/nanodbc/nanodbc/pull/191

--- src/nanodbc/nanodbc.cpp
+++ src/nanodbc/nanodbc.cpp
@@ -879,10 +879,21 @@ public:
         if (!success(rc))
             NANODBC_THROW_DATABASE_ERROR(env_, SQL_HANDLE_ENV);
 
-        NANODBC_CALL_RC(
-            SQLSetConnectAttr, rc, conn_, SQL_LOGIN_TIMEOUT, (SQLPOINTER)(std::intptr_t)timeout, 0);
-        if (!success(rc))
-            NANODBC_THROW_DATABASE_ERROR(conn_, SQL_HANDLE_DBC);
+        if (timeout != 0)
+        {
+            // Avoid to set the timeout to 0 (no timeout).
+            // This is a workaround for the Oracle ODBC Driver (11.1), as this
+            // operation is not supported by the Driver.
+            NANODBC_CALL_RC(
+                SQLSetConnectAttr,
+                rc,
+                conn_,
+                SQL_LOGIN_TIMEOUT,
+                (SQLPOINTER)(std::intptr_t)timeout,
+                0);
+            if (!success(rc))
+                NANODBC_THROW_DATABASE_ERROR(conn_, SQL_HANDLE_DBC);
+        }
 
 #if !defined(NANODBC_DISABLE_ASYNC) && defined(SQL_ATTR_ASYNC_DBC_EVENT)
         if (event_handle != nullptr)
@@ -921,10 +932,21 @@ public:
         if (!success(rc))
             NANODBC_THROW_DATABASE_ERROR(env_, SQL_HANDLE_ENV);
 
-        NANODBC_CALL_RC(
-            SQLSetConnectAttr, rc, conn_, SQL_LOGIN_TIMEOUT, (SQLPOINTER)(std::intptr_t)timeout, 0);
-        if (!success(rc))
-            NANODBC_THROW_DATABASE_ERROR(conn_, SQL_HANDLE_DBC);
+        if (timeout != 0)
+        {
+            // Avoid to set the timeout to 0 (no timeout).
+            // This is a workaround for the Oracle ODBC Driver (11.1), as this
+            // operation is not supported by the Driver.
+            NANODBC_CALL_RC(
+                SQLSetConnectAttr,
+                rc,
+                conn_,
+                SQL_LOGIN_TIMEOUT,
+                (SQLPOINTER)(std::intptr_t)timeout,
+                0);
+            if (!success(rc))
+                NANODBC_THROW_DATABASE_ERROR(conn_, SQL_HANDLE_DBC);
+        }
 
 #if !defined(NANODBC_DISABLE_ASYNC) && defined(SQL_ATTR_ASYNC_DBC_EVENT)
         if (event_handle != nullptr)
