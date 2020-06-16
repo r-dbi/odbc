da7b4859ae7bd0c9760e48a9dca133e22562c53d
Jim Hester <james.f.hester@gmail.com>
2020-06-16 16:07:48 -0400

    Include installation code

---
+++ .github/odbc/install-mariadb-driver.sh
@@ -0,0 +1,14 @@
+# from https://mariadb.com/kb/en/about-mariadb-connector-odbc/#installing-mariadb-connectorodbc-on-debianubuntu
+mkdir odbc_package
+cd odbc_package
+wget https://downloads.mariadb.com/Connectors/odbc/connector-odbc-3.1.7/mariadb-connector-odbc-3.1.7-ga-debian-x86_64.tar.gz
+tar -xvzf mariadb-connector-odbc-3.1.7-ga-debian-x86_64.tar.gz
+sudo install lib64/libmaodbc.so /usr/lib/
+sudo install -d /usr/lib/mariadb/
+sudo install -d /usr/lib/mariadb/plugin/
+sudo install lib/mariadb/plugin/auth_gssapi_client.so /usr/lib/mariadb/plugin/
+sudo install lib/mariadb/plugin/caching_sha2_password.so /usr/lib/mariadb/plugin/
+sudo install lib/mariadb/plugin/client_ed25519.so /usr/lib/mariadb/plugin/
+sudo install lib/mariadb/plugin/dialog.so /usr/lib/mariadb/plugin/
+sudo install lib/mariadb/plugin/mysql_clear_password.so /usr/lib/mariadb/plugin/
+sudo install lib/mariadb/plugin/sha256_password.so /usr/lib/mariadb/plugin/
