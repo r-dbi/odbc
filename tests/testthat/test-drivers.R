e64545deda8764b90846a3ce67cb6bc901136f34
Jim Hester <james.f.hester@gmail.com>
2020-10-16 14:06:21 -0400

    Add skips for the drivers test

--- tests/testthat/test-drivers.R
+++ tests/testthat/test-drivers.R
@@ -3,6 +3,10 @@ context("drivers")
 test_that("odbcListDrivers() returns available drivers", {
   skip_on_cran()
   res <- odbcListDrivers()
+  if (nrow(res) == 0) {
+    skip("No drivers installed")
+  }
+
   expect_identical(names(res), c("name", "attribute", "value"))
   expect_true(nrow(res) >= 1)
 })
@@ -11,6 +15,11 @@ test_that("odbcListDrivers() keep and filter work", {
   skip_on_cran()
 
   current_drivers <- odbcListDrivers()[["name"]]
+
+  if (length(current_drivers) == 0) {
+    skip("No drivers installed")
+  }
+
   res <- odbcListDrivers(filter = current_drivers)
   expect_true(nrow(res) == 0)
 
@@ -20,7 +29,12 @@ test_that("odbcListDrivers() keep and filter work", {
 
 test_that("odbcListDataSources() returns available data sources", {
   skip_on_cran()
+
   res <- odbcListDataSources()
+  if (nrow(res) == 0) {
+    skip("No drivers installed")
+  }
+
   expect_identical(names(res), c("name", "description"))
   expect_true(nrow(res) >= 1)
 })
