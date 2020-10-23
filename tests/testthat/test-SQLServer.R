044dd9188275b5108e526b5e72bf76cfe85239bd
Jim Hester <james.f.hester@gmail.com>
2020-10-23 12:40:13 -0400

    Turn on more SQL Server tests

--- tests/testthat/test-SQLServer.R
+++ tests/testthat/test-SQLServer.R
@@ -7,7 +7,6 @@ test_that("SQLServer", {
       "package_name", # Not an error
       NULL))
   DBItest::test_driver(c(
-    "connect_format",
     "connect_bigint_integer",
     "connect_bigint_character",
     "connect_bigint_integer64",
@@ -15,23 +14,15 @@ test_that("SQLServer", {
   DBItest::test_connection(c(
       NULL))
   DBItest::test_result(c(
-      "fetch_n_bad",                     # TODO
-      "fetch_n_good_after_bad",          # TODO
-      "fetch_no_return_value",           # TODO
-      "get_query_n_bad",                     # todo
-      "get_query_good_after_bad_n",                     # todo
-      "get_query_n_zero_rows",                     # todo
-      "get_query_n_multi_row_inf",
+      "get_query_n_zero_rows",
       "get_query_n_incomplete",
-      "get_query_params",
       "fetch_no_return_value",           # TODO
-      "fetch_n_multi_row_inf",
-      ".*clear_result_.*_statement", # TODO
+      "clear_result_return_statement",
+      "cannot_clear_result_twice_statement",
       "send_statement.*", # Invalid CTAS syntax
       "execute_atomic", # Invalid CTAS syntax
       "execute_immediate", # Invalid CTAS syntax
       "data_character", # I think the test is bad
-      "data_logical$", # Not an error
       "data_64_bit_numeric_warning", # Test does not explicitly set 64 bit columns
       "data_64_bit_lossless", # Test does not explicitly set 64 bit columns
       "data_date.*", # Date not a builtin function name
@@ -42,38 +33,21 @@ test_that("SQLServer", {
   DBItest::test_sql(c(
       "append_roundtrip_.*", # TODO
       "quote_string_na_is_null", # Invalid syntax
-      "quote_identifier_vectorized", # TODO
-      "quote_identifier", # Invalid Syntax
-      "quote_identifier_special", # TODO
-      "remove_table_temporary_arg",
       "remove_table_missing_succeed",
-      "remove_table_missing_succeed",
-      "temporary_table", # Unsupported
       "roundtrip_character", # #10
       "roundtrip_character_native", # Possible false positive
       "roundtrip_factor", # #10
       "roundtrip_time", #TODO
-      "roundtrip_date", # unsupported
-      "roundtrip_quotes", # TODO, not sure why this is failing
       "roundtrip_timestamp", # We explicitly want to set tzone to UTC regardless of input
       "write_table_error", # TODO
-      "list_tables", # TODO
-      ".*_table_name", # TODO
-      "append_table_error", # TODO
       "quote_string_roundtrip",
       "quote_literal_roundtrip",
       "quote_literal_na_is_null",
       "quote_literal_na_is_null",
-      "unquote_identifier_vectorized",
-      "read_table_empty",
       "create_table_error",
       "create_temporary_table",
-      "write_table_append_incompatible",
       "roundtrip_64_bit_roundtrip",
-      "roundtrip_field_types",
       "write_table_row_names_default",
-      "remove_table_temporary",
-      "list_objects_features",
       "list_fields_wrong_table",
       "list_fields_quoted",
       "list_fields_object",
