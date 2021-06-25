f967a44d9e3c95c838f8f7163cfeb1b1a3fdfd93
sureframe846 <57026394+sureframe846@users.noreply.github.com>
2021-06-25 17:34:56 -0400

    add dbExistsTable and dbListTables for DB2

--- R/driver-db2.R
+++ R/driver-db2.R
@@ -29,3 +29,43 @@ setMethod("sqlCreateTable", "DB2/AIX64",
     ))
   }
 )
+
+## Find temp tables in dbExistsTable and dbListTables on DB2
+
+setMethod("dbExistsTable", c("DB2/AIX64", "Id"),
+  function(conn, name, ...) {
+    tryCatch(expr = {
+      temptables = dbGetQuery(conn, "SELECT TABSCHEMA, TABNAME FROM SYSIBMADM.ADMINTEMPTABLES")
+      # trim whitespace because sometimes schema are saved in the above table with extra whitespace
+      return(any(trimws(temptables$TABNAME) == toupper(id_field(name, "table")) & 
+                   trimws(temptables$TABSCHEMA == toupper(id_field(name, "schema")))) |
+               name@name[["table"]] %in%
+               connection_sql_tables(conn@ptr,
+                                     catalog_name = id_field(name, "catalog"),
+                                     schema_name = id_field(name, "schema"),
+                                     table_name = id_field(name, "table"))
+             )
+      },
+      error = function(cond) {
+        return(name@name[["table"]] %in%
+                 connection_sql_tables(conn@ptr,
+                                       catalog_name = id_field(name, "catalog"),
+                                       schema_name = id_field(name, "schema"),
+                                       table_name = id_field(name, "table"))
+               )
+      }
+    )
+  }
+)
+
+setMethod("dbListTables", "DB2/AIX64",
+  function(conn, ...) {
+    tryCatch(return(c(dbGetQuery("SELECT TABNAME FROM SYSIBMADM.ADMINTEMPTABLES")[["TABNAME"]],
+                      connection_sql_tables(conn@ptr)$table_name)
+                    ),
+             error = function(cond) {
+               return(connection_sql_tables(conn@ptr)$table_name)
+              }
+             )
+  }
+)
