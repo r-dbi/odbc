12ab037a8ed98fd381d3ac51ed67033958b2e993
detule <ogjoneski@gmail.com>
2023-01-22 09:48:17 -0500

    tests: drivers: skip if no data sources (#534)

--- tests/testthat/test-drivers.R
+++ tests/testthat/test-drivers.R
@@ -26,6 +26,9 @@ test_that("odbcListDataSources() returns available data sources", {
   skip_if_no_drivers()
 
   res <- odbcListDataSources()
+  if (nrow(res) == 0) {
+    skip("No odbc data-sources configured")
+  }
   expect_identical(names(res), c("name", "description"))
   expect_true(nrow(res) >= 1)
 })
