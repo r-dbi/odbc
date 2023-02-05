d49fd88fe39d003ea2e08b2e4989e82da6775b58
Xavier DUQUESNE <xavier.duquesne@enedis.fr>
2023-02-05 12:59:04 +0100

    fix dbExistsTable and sqlCreateTable for Teradata
    Temporary (volatile) tables in Teradata are not found by `connection_sql_tables`. The edge case was not handled by `dbExistsTable`. A specific SQL query needs to be run.
    
    sqlCreateTable needs additional arguments to be useful (index, partition_by). In my company, most tables are specified as "multiset". It is added as the default.

--- R/db.R
+++ R/db.R
@@ -18,16 +18,138 @@ setMethod("sqlCreateTable", "Oracle",
 
 setClass("Teradata", where = class_cache)
 
-setMethod("sqlCreateTable", "Teradata",
-  function(con, table, fields, field.types = NULL, row.names = NA, temporary = FALSE, ...) {
-    table <- dbQuoteIdentifier(con, table)
-    fields <- createFields(con, fields, field.types, row.names)
+sql_create_table_teradata <- function(
+  con, table, fields, field.types = NULL, row.names = NA, temporary = FALSE,
+  # teradata specific arguments
+  index = NA,
+  partition_by = NA,
+  ...) {
 
-    SQL(paste0(
-        "CREATE ", if (temporary) " MULTISET VOLATILE ", "TABLE ", table, " (\n",
-        "  ", paste(fields, collapse = ",\n  "), "\n)\n", if (temporary) " ON COMMIT PRESERVE ROWS"
-        ))
-  })
+  table <- dbQuoteIdentifier(con, table)
+  fields <- createFields(con, fields, field.types, row.names)
+
+  DBI::SQL(paste0(
+    "CREATE MULTISET ",
+    if (temporary) "VOLATILE ",
+    "TABLE ", table,
+    if(temporary) ", no log", " (\n",
+    "  ", paste(fields, collapse = ",\n  "), "\n)\n",
+    if(!missing(index)) paste0("PRIMARY INDEX ( ", paste(index, collapse = ", "), " ) "), "\n",
+    if(!missing(partition_by)) paste0(partition_by, "\n"),
+    if (temporary) "ON COMMIT PRESERVE ROWS"
+  ))
+}
+
+
+#' @rdname odbc-tables
+#' @param index (teradata specific) character vector of columns to put in the `PRIMARY INDEX` clause.
+#' @param partition_by (teradata specific) character vector of length 1, that is put between
+#'                     `PRIMARY INDEX` and before `ON COMMIT PRESERVE ROWS` clause.
+#'                     It should start with "PARTITION BY".
+#'
+#' @export
+setMethod(
+  "sqlCreateTable", c("TeradataSTM", "character"),
+  sql_create_table_teradata
+)
+
+#' @export
+setMethod(
+  "sqlCreateTable", c("TeradataSTM", "SQL"),
+  sql_create_table_teradata
+)
+
+#' @export
+setMethod(
+  "sqlCreateTable", c("TeradataSTM", "Id"),
+  sql_create_table_teradata
+)
+
+# check if table is "temporary" (has no schema specified)
+teradata_is_temporary_table_name <- function(table_name){
+  if (is(table_name, "Id")){
+    !("schema" %in% names(table_name@name))
+  } else if(is(table_name, "SQL")){
+    !grepl("\\.", table_name)
+  } else if(is.character(table_name)){
+    !grepl("\\.", table_name)
+  } else{
+    stop("table_name must be DBI::Id, character or SQL", call. = FALSE)
+  }
+}
+
+teradata_exists_table <- function(conn, name, temporary = NULL, schema_name, ...) {
+  stopifnot(length(name) == 1)
+
+  if(is.null(temporary)){
+    if(missing(schema_name)){
+      temporary <- teradata_is_temporary_table_name(name)
+    } else {
+      temporary <- FALSE
+    }
+  }
+
+  # significantly faster to only look in volatile tables if looking for
+  # volatile tables.
+  if(temporary){
+    name %in% DBI::dbGetQuery(conn, "HELP VOLATILE TABLE")[["Table SQL Name"]]
+  } else {
+    name %in% DBI::dbListTables(conn, table_name = name, schema_name = schema_name, ...)
+  }
+}
+
+#' Teradata version of dbExistsTable
+#'
+#' @inheritParams DBI::dbExistsTable
+#' @param temporary (boolean, default to NULL) if TRUE, look only into temporary table.
+#'                  Setting `temporary` to FALSE allow to look for a table name in multiple schemas.
+#'                  `temporary` is ignored if an object of type `DBI::Id` is passed
+#'                  to `name`.
+#' @param ... Other arguments are passed to DBI::dbListTables
+#'
+#' @export
+#' @rdname dbExistsTable_teradata
+setMethod(
+  "dbExistsTable", c("Teradata", "character"),
+  teradata_exists_table
+)
+
+#' @export
+#' @rdname dbExistsTable_teradata
+setMethod(
+  "dbExistsTable", c("Teradata", "Id"),
+  function(conn, name, temporary = NULL, ...){
+    teradata_exists_table(
+      conn,
+      name = id_field(name, "table"),
+      temporary = teradata_is_temporary_table_name(name),
+      schema_name = id_field(name, "schema"),
+      ...
+    )
+  }
+)
+
+#' @export
+#' @rdname dbExistsTable_teradata
+setMethod(
+  "dbExistsTable", c("Teradata", "SQL"),
+  function(conn, name, ...){
+
+    name_vec <- strsplit(name, ".", fixed = TRUE)[[1]]
+    if(length(name_vec) > 2){
+      stop("More than 1 dot found in argument `name`,
+           cannot identify schema and table in argument `name`.")
+    }
+    teradata_exists_table(
+      conn,
+      name = name_vec[2],
+      temporary = FALSE,
+      schema_name = name_vec[1],
+      ...
+    )
+  }
+)
+
 
 setMethod(
   "dbListTables", "Teradata",
