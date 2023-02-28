64c24b67216a2250e3afa00db0e9979b04cc142a
detule <ogjoneski@gmail.com>
2023-02-27 19:54:11 -0500

    CI: Build mysql connector (#548)

--- .github/odbc/install-mariadb-driver.sh
+++ .github/odbc/install-mariadb-driver.sh
@@ -1,16 +1,9 @@
-# from https://mariadb.com/kb/en/about-mariadb-connector-odbc/#installing-mariadb-connectorodbc-on-debianubuntu
-mkdir odbc_package
-pushd odbc_package
-wget https://downloads.mariadb.com/Connectors/odbc/connector-odbc-3.1.7/mariadb-connector-odbc-3.1.7-ga-debian-x86_64.tar.gz
-tar -xvzf mariadb-connector-odbc-3.1.7-ga-debian-x86_64.tar.gz
-sudo install lib/libmaodbc.so /usr/lib/
-sudo install -d /usr/lib/mariadb/
-sudo install -d /usr/lib/mariadb/plugin/
-sudo install lib/mariadb/plugin/auth_gssapi_client.so /usr/lib/mariadb/plugin/
-sudo install lib/mariadb/plugin/caching_sha2_password.so /usr/lib/mariadb/plugin/
-sudo install lib/mariadb/plugin/client_ed25519.so /usr/lib/mariadb/plugin/
-sudo install lib/mariadb/plugin/dialog.so /usr/lib/mariadb/plugin/
-sudo install lib/mariadb/plugin/mysql_clear_password.so /usr/lib/mariadb/plugin/
-sudo install lib/mariadb/plugin/sha256_password.so /usr/lib/mariadb/plugin/
-popd
-rm -r odbc_package
+sudo apt-get install -y cmake
+cd /tmp && git clone https://github.com/MariaDB/mariadb-connector-odbc.git connector
+cd connector && git checkout 3.1.17
+mkdir build && cd build
+cmake ../ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCONC_WITH_UNIT_TESTS=Off -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_SSL=OPENSSL
+cmake --build . --config RelWithDebInfo
+sudo make install
+sudo ln -s /usr/local/lib/mariadb/libmariadb.so.3 /usr/local/lib/
+sudo ldconfig
