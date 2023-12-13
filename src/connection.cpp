ad62e790b8346bd129b11d7d06b14913f2cb4edc
Gábor Csárdi <csardi.gabor@gmail.com>
2023-12-13 18:18:27 +0100

    Use SQLINTEGER in get_info()
    Instead of `unsigned short`, which could be allocated
    or aligned differently by different compilers.
    
    There are other places in nanodbc that use `unsigned short`
    in the protocol, so potentially those are broken as well.
    
    I am not very familiar with the ODBC toolchain, so it
    is possible that this is not the best fix, but nevertheless
    this is the step that causes the memory error and the
    crash, so this needs to change.
    
    For #624.

--- src/connection.cpp
+++ src/connection.cpp
@@ -75,10 +75,10 @@ std::string get_info_or_empty(connection_ptr const& p, short type) {
 
 // [[Rcpp::export]]
 Rcpp::List connection_info(connection_ptr const& p) {
-  unsigned short getdata_ext;
+  SQLINTEGER getdata_ext;
   try {
     getdata_ext =
-        (*p)->connection()->get_info<unsigned short>(SQL_GETDATA_EXTENSIONS);
+        (*p)->connection()->get_info<SQLINTEGER>(SQL_GETDATA_EXTENSIONS);
   } catch (const nanodbc::database_error& c) {
     getdata_ext = 0;
   }
