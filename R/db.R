21beed73757aac983ac002767e892624136a3706
Hadley Wickham <h.wickham@gmail.com>
2023-12-15 08:13:15 -0600

    Pull out DB2

--- R/db.R
+++ R/db.R
@@ -293,32 +293,3 @@ setMethod("odbcConnectionSchemas", c("Spark SQL", "character"),
 )
 
 # DB2 ----------------------------------------------------------------
-
-#' @export
-#' @rdname DBI-classes
-setClass("DB2/AIX64", contains = "OdbcConnection")
-
-#' @rdname DBI-methods
-# Don't generate a usage because there's some buglet in R CMD check
-# (probably because of the `/` in the class name) which flags a usage
-# without corresponding alias
-#' @usage NULL
-setMethod("sqlCreateTable", "DB2/AIX64",
-  function(con,
-           table,
-           fields,
-           row.names = NA,
-           temporary = FALSE,
-           ...,
-           field.types = NULL) {
-    table <- dbQuoteIdentifier(con, table)
-    fields <- createFields(con, fields, field.types, row.names)
-
-    SQL(paste0(
-      if (temporary) "DECLARE GLOBAL TEMPORARY" else "CREATE",
-      " TABLE ", table, " (\n",
-      "  ", paste(fields, collapse = ",\n  "),
-      "\n)\n", if (temporary) " ON COMMIT PRESERVE ROWS"
-    ))
-  }
-)
