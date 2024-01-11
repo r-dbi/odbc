ec4fc7b36278d83605c12771a90fcba12056fe3d
Hadley Wickham <h.wickham@gmail.com>
2024-01-11 08:35:52 -0600

    Teach id_field() to work with unnamed Ids

--- R/utils.R
+++ R/utils.R
@@ -54,10 +54,26 @@ parse_size <- function(x) {
 }
 
 id_field <- function(id, field, default = NULL) {
-  if (field %in% names(id@name)) {
-    id@name[[field]]
+  if (length(id@name) == 1) {
+    switch(field,
+      catalog = NULL,
+      schema = NULL,
+      table = id@name[[1]],
+    )
+  } else if (length(id@name) == 2) {
+    switch(field,
+      catalog = NULL,
+      schema = id@name[[1]],
+      table = id@name[[2]],
+    )
+  } else if (length(id@name) == 3) {
+    switch(field,
+      catalog = id@name[[1]],
+      schema = id@name[[2]],
+      table = id@name[[3]],
+    )
   } else {
-    default
+    cli_abort("Idenifier must be length 1, 2, or 3.")
   }
 }
 
