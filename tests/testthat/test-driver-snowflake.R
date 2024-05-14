ae9451737148838eb5833f88c651fc0020d46802
simonpcouch <simonpatrickcouch@gmail.com>
2024-05-14 12:49:46 -0500

    skip snowflake `test_con()` when repository secret not available

--- tests/testthat/test-driver-snowflake.R
+++ tests/testthat/test-driver-snowflake.R
@@ -1,3 +1,8 @@
 test_that("can connect to snowflake", {
+  pwd <- Sys.getenv("ODBC_PWD_SNOWFLAKE")
+  if (nchar(pwd) == 0) {
+    skip("Secret ODBC_PWD_SNOWFLAKE not available.")
+  }
+
   con <- test_con("SNOWFLAKE")
 })
