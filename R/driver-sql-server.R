50cdee8843080291f630e6fba95bd49da3dd1b98
Aaron Jacobs <aaron.jacobs@rstudio.com>
2025-02-28 14:07:36 -0500

    WIP Synapse support.

--- R/driver-sql-server.R
+++ R/driver-sql-server.R
@@ -262,3 +262,211 @@ setMethod("odbcConnectionColumns_", c("Microsoft SQL Server", "SQL"),
     odbcConnectionColumns_(conn, dbUnquoteIdentifier(conn, name)[[1]], ...)
   }
 )
+
+#' Helper for Connecting to Azure Synapse via ODBC
+#'
+#' @description
+#'
+#' Connect to Synapse clusters and SQL warehouses via the [Synapse ODBC
+#' driver](https://www.synapse.com/spark/odbc-drivers-download).
+#'
+#' In particular, the custom `dbConnect()` method for the Synapse ODBC driver
+#' implements a subset of the [Synapse client unified authentication](https://docs.synapse.com/en/dev-tools/auth.html#synapse-client-unified-authentication)
+#' model, with support for personal access tokens, OAuth machine-to-machine
+#' credentials, and OAuth user-to-machine credentials supplied via Posit
+#' Workbench or the Synapse CLI on desktop. It can also detect viewer-based
+#' credentials on Posit Connect if the \pkg{connectcreds} package is
+#' installed. All of these credentials are detected automatically if present
+#' using [standard environment variables](https://docs.synapse.com/en/dev-tools/auth.html#environment-variables-and-fields-for-client-unified-authentication).
+#'
+#' @inheritParams DBI::dbConnect
+#' @param server A fully-qualified Synapse server name, e.g.
+#'   `"<server-name>.sql.azuresynapse.net"` (for dedicated pools) or
+#'   `"<server-name>-ondemand.sql.azuresynapse.net"` (for serverless pools)
+#' @param database The name of the Synapse database to connect to.
+#' @param driver The name of the Microsoft SQL Server ODBC driver, or `NULL` to
+#'   use the default name.
+#' @param uid,pwd A username and password to use for authentication. Or omit
+#'   these to use Entra ID authentication instead, which is generally more
+#'   secure.
+#' @param ... Further arguments passed on to [`dbConnect()`].
+#'
+#' @returns An `OdbcConnection` object with an active connection to a Synapse
+#'   cluster or SQL warehouse.
+#'
+#' @examples
+#' \dontrun{
+#' DBI::dbConnect(
+#'   odbc::synapse(),
+#'   httpPath = "sql/protocolv1/o/4425955464597947/1026-023828-vn51jugj"
+#' )
+#'
+#' # Use credentials from the viewer (when possible) in a Shiny app
+#' # deployed to Posit Connect.
+#' library(connectcreds)
+#' server <- function(input, output, session) {
+#'   conn <- DBI::dbConnect(
+#'     odbc::synapse(),
+#'     httpPath = "sql/protocolv1/o/4425955464597947/1026-023828-vn51jugj"
+#'   )
+#' }
+#' }
+#' @export
+synapse <- function() {
+  new("SynapseOdbcDriver")
+}
+
+#' @rdname synapse
+#' @export
+setClass("SynapseOdbcDriver", contains = "OdbcDriver")
+
+#' @rdname synapse
+#' @export
+setMethod("dbConnect", "SynapseOdbcDriver",
+  function(drv,
+           # TODO: Can we use a Synapse workspace here somehow instead?
+           # See: https://learn.microsoft.com/en-us/azure/synapse-analytics/sql/connect-overview
+           server,
+           database,
+           driver = NULL,
+           uid = NULL,
+           pwd = NULL,
+           ...) {
+    call <- caller_env()
+    check_string(server, allow_empty = FALSE, call = call)
+    check_string(database, allow_empty = FALSE, call = call)
+    check_string(driver, allow_null = TRUE, call = call)
+    check_string(uid, allow_null = TRUE, call = call)
+    check_string(pwd, allow_null = TRUE, call = call)
+    args <- synapse_args(
+      server = server,
+      database = database,
+      driver = driver,
+      uid = uid,
+      pwd = pwd,
+      ...,
+      call = call
+    )
+    inject(dbConnect(odbc(), !!!args))
+  }
+)
+
+synapse_args <- function(
+  driver = NULL,
+  uid = NULL,
+  pwd = NULL,
+  ...,
+  call = current_env()
+) {
+  default_args <- list(
+    driver = driver %||% synapse_default_driver(call = call),
+    encrypt = "yes",
+    port = 1433L
+  )
+  auth_args <- synapse_auth_args(uid = uid, pwd = pwd, call = call)
+  utils::modifyList(c(default_args, auth_args), list(...))
+}
+
+# Returns a sensible driver name even if odbc.ini and odbcinst.ini do not
+# contain an entry for the Synapse ODBC driver. For Linux and macOS we
+# default to known shared library paths used by the official installers.
+# On Windows we use the official driver name.
+synapse_default_driver <- function(call = current_env()) {
+  find_default_driver(
+    synapse_default_driver_paths(),
+    # FIXME
+    fallbacks = c("Synapse", "Simba Spark ODBC Driver"),
+    label = "SQL Server",
+    call = call
+  )
+}
+
+# FIXME
+synapse_default_driver_paths <- function() {
+  if (Sys.info()["sysname"] == "Linux") {
+    paths <- Sys.glob(c(
+      "/opt/rstudio-drivers/spark/bin/lib/libsparkodbc_sb*.so",
+      "/opt/simba/spark/lib/64/libsparkodbc_sb*.so"
+    ))
+  } else if (Sys.info()["sysname"] == "Darwin") {
+    paths <- Sys.glob("/Library/simba/spark/lib/libsparkodbc_sb*.dylib")
+  } else {
+    paths <- character()
+  }
+  paths
+}
+
+synapse_auth_args <- function(host, uid = NULL, pwd = NULL, call = current_env()) {
+  # Detect viewer-based credentials from Posit Connect.
+  #
+  # TODO: Is this actually the right scope?
+  scope <- "https://database.windows.net/.default"
+  if (is_installed("connectcreds")) {
+    if (connectcreds::has_viewer_token(scope = scope)) {
+      token <- connectcreds::connect_viewer_token(scope = scope)
+      return(list(attributes = list("azure_token" = token$access_token)))
+    }
+  }
+
+  if (!is.null(uid) && !is.null(pwd)) {
+    return(list(uid = uid, pwd = pwd, authentication = "SqlPassword"))
+  } else if (xor(is.null(uid), is.null(pwd))) {
+    cli::cli_abort(
+      c(
+        "Both {.arg uid} and {.arg pwd} must be specified for username and \
+        password authentication.",
+        i = "Or leave both unset to use Entra ID authentication."
+      ),
+      call = call
+    )
+  }
+
+  # Detect Azure service principals.
+  client_id <- Sys.getenv("AZURE_CLIENT_ID")
+  client_secret <- Sys.getenv("AZURE_CLIENT_SECRET")
+  if (nchar(client_id) && nchar(client_secret)) {
+    return(list(
+      uid = client_id,
+      pwd = client_secret,
+      authentication = "ActiveDirectoryServicePrincipal"
+    ))
+  }
+
+  # Masquerade as the Azure CLI for interactive authentication.
+  client_id <- "04b07795-8ddb-461a-bbee-02f9e1bf7b46"
+  if (is_interactive() && !is_hosted_session()) {
+    check_installed("httr2", "for interactive Entra ID authentication")
+    client <- httr2::oauth_client(
+      client_id,
+      token_url = "https://login.microsoftonline.com/common/oauth2/v2.0/token",
+      secret = "",
+      auth = "body",
+      name = paste0("odbc-", client_id)
+    )
+    token <- httr2::oauth_token_cached(
+      client,
+      oauth_flow_auth_code,
+      flow_params = list(
+        auth_url = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
+        scope = paste(scope, "offline_access"),
+        redirect_uri = "http://localhost:8400",
+        auth_params = list(prompt = "select_account")
+      )
+    )
+    return(list(attributes = list("azure_token" = token$access_token)))
+  }
+
+  cli::cli_abort(
+    c(
+      "No Entra ID credentials available.",
+      "i" = "Supply {.arg uid} and {.arg pwd} to authenticate with a username \
+             and password.",
+      "i" = if (running_on_connect()) {
+        "Or consider enabling Posit Connect's Synapse integration for
+        viewer-based credentials. See {.url https://docs.posit.co/connect/user/oauth-integrations/#adding-oauth-integrations-to-deployed-content}
+        for details."
+      }
+    ),
+    call = call
+  )
+}
