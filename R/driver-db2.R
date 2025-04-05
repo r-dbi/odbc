ede42ce53b443449f682391ce2d142bb077cde0d
detule <ogjoneski@gmail.com>
2025-04-05 17:23:05 +0000

    db2: Fixup temp table lookup

--- R/driver-db2.R
+++ R/driver-db2.R
@@ -68,7 +68,9 @@ setMethod("odbcConnectionTables", c("DB2/AIX64", "character"),
       tryCatch({
         dfTempTables <- dbGetQuery(conn, query)
         # NULL as colname is not well liked by DB2 it seems. Hack here.
-        dfTempTables$table_remarks <- NA_character_
+        if (nrow(dfTempTables)) {
+          dfTempTables$table_remarks <- NA_character_
+        }
       }, odbc_database_error = function(e) {
         cli::cli_warn("Unable to query for temporary tables in SYSIBMADM.ADMINTEMPTABLES")
       })
