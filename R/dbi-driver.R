3f8567f7112e1550d691cdd84b8be175465cea09
detule <ogjoneski@gmail.com>
2025-11-29 02:10:53 -0800

    man: figure/width css fixup (#967)

--- R/dbi-driver.R
+++ R/dbi-driver.R
@@ -120,7 +120,7 @@ setMethod("show", "OdbcDriver",
 #' ![A diagram containing four boxes with arrows linking each pointing left to
 #' right. The boxes read, in order, R interface, driver manager, ODBC driver,
 #' and database. The left-most box, R interface, contains three smaller
-#' components, labeled dbplyr, DBI, and odbc.](whole-game.png){options: width=95%}
+#' components, labeled dbplyr, DBI, and odbc.](whole-game.png){options: style="width: 95%;"}
 #'
 #' The package supports any **Database Management System (DBMS)** with ODBC
 #' support. Support for a given DBMS is provided by an **ODBC driver**, which
