a6936903a1ef3569fa4a355206669e3dbb41cb52
detule <ogjoneski@gmail.com>
2026-04-30 22:16:33 -0400

    snowflake: sf_private_key: fixup buffer allocation (#997)
    * snowflake: sf_private_key: allocate a buffer large enough to hold null terminator
    
    * Apply suggestion from @simonpcouch
    
    Co-authored-by: Simon P. Couch <simonpatrickcouch@gmail.com>
    
    * update NEWS
    
    ---------
    
    Co-authored-by: Simon P. Couch <simonpatrickcouch@gmail.com>

--- src/utils.cpp
+++ src/utils.cpp
@@ -60,7 +60,7 @@ namespace utils {
       {
         std::shared_ptr<std::string> priv_key =
           std::make_shared<std::string>(Rcpp::as<std::string>(r_attributes["sf_private_key"]));
-        std::shared_ptr< void > buffer(malloc(priv_key->size()), std::free);
+        std::shared_ptr< void > buffer(malloc(priv_key->size() + 1), std::free);
         // Copy null terminator as well
         std::memcpy(buffer.get(), priv_key->c_str(), priv_key->size() + 1);
         attributes.push_back(nanodbc::connection::attribute(
@@ -72,7 +72,7 @@ namespace utils {
       {
         std::shared_ptr<std::string> key_pass =
           std::make_shared<std::string>(Rcpp::as<std::string>(r_attributes["sf_private_key_password"]));
-        std::shared_ptr< void > buffer(malloc(key_pass->size()), std::free);
+        std::shared_ptr< void > buffer(malloc(key_pass->size() + 1), std::free);
         // Copy null terminator as well
         std::memcpy(buffer.get(), key_pass->c_str(), key_pass->size() + 1);
         attributes.push_back(nanodbc::connection::attribute(
